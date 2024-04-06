5026b59d7e4ded1020333e7c347a97264bc21983
lzlocki <lzlocki@poczta.fm>
2024-04-06 09:56:52 +0200

    Add getters and setters

--- Hotel/src/main/java/pl/wsb/hotel/Room.java
+++ Hotel/src/main/java/pl/wsb/hotel/Room.java
@@ -12,7 +12,7 @@ public class Room {
     private boolean hasTV;
     private boolean hasWifi;
 
-    // constructor
+    // constructor //
     public Room(String id, String description, double area, int floor, boolean hasKingSizeBed, boolean hasBalcony, boolean hasFridge, int numberOfBeds, boolean hasTV, boolean hasWifi) {
         this.id = id;
         this.description = description;
@@ -23,6 +23,62 @@ public class Room {
         this.hasFridge = hasFridge;
         this.numberOfBeds = numberOfBeds;
         this.hasTV = hasTV;
+        this.hasWifi = hasWifi;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public boolean hasKingSizeBed() {
+        return hasKingSizeBed;
+    }
+
+    public boolean hasBalcony() {
+        return hasBalcony;
+    }
+
+    public boolean hasFridge() {
+        return hasFridge;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public boolean hasTV() {
+        return hasTV;
+    }
+
+    public void setHasTV(boolean hasTV) {
+        this.hasTV = hasTV;
+    }
+
+    public boolean hasWifi() {
+        return hasWifi;
+    }
+
+    public void setHasWifi(boolean hasWifi) {
         this.hasWifi = hasWifi;
     }
 }
