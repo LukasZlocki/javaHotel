16d1ac6a54a33f5f415b50206ccfd3489897e40d
lzlocki <lzlocki@poczta.fm>
2024-04-06 09:55:20 +0200

    Add additional fields and modify class constructor

--- Hotel/src/main/java/pl/wsb/hotel/Room.java
+++ Hotel/src/main/java/pl/wsb/hotel/Room.java
@@ -6,13 +6,23 @@ public class Room {
     private  double area;
     private int floor;
     private boolean hasKingSizeBed;
+    private boolean hasBalcony;
+    private boolean hasFridge;
+    private int numberOfBeds;
+    private boolean hasTV;
+    private boolean hasWifi;
 
     // constructor
-    public Room(String id, String description, double area, int floor, boolean hasKingSizeBed){
+    public Room(String id, String description, double area, int floor, boolean hasKingSizeBed, boolean hasBalcony, boolean hasFridge, int numberOfBeds, boolean hasTV, boolean hasWifi) {
         this.id = id;
         this.description = description;
         this.area = area;
         this.floor = floor;
         this.hasKingSizeBed = hasKingSizeBed;
+        this.hasBalcony = hasBalcony;
+        this.hasFridge = hasFridge;
+        this.numberOfBeds = numberOfBeds;
+        this.hasTV = hasTV;
+        this.hasWifi = hasWifi;
     }
 }
