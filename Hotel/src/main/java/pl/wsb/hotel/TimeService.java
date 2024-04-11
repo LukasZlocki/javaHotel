1d1bf71ec3505de4413ef7c92b04e32a23c2aa06
lzlocki <lzlocki@poczta.fm>
2024-04-11 18:17:47 +0200

    Add TimeService class

---
+++ Hotel/src/main/java/pl/wsb/hotel/TimeService.java
@@ -0,0 +1,18 @@
+package pl.wsb.hotel;
+
+public class TimeService extends SpecialService{
+    @Override
+    public void orderService() {
+        // code body here... //
+    }
+
+    @Override
+    public void orderClothesCleaning() {
+        // code body here... //
+    }
+
+    @Override
+    public void orderNetflix() {
+        // code body here... //
+    }
+}
