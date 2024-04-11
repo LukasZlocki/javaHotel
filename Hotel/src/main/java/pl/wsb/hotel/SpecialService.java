89af6234df4345cdbb8a5c53d37dc07ba28716ab
lzlocki <lzlocki@poczta.fm>
2024-04-11 22:43:12 +0200

    Remove old and add new additional services

--- Hotel/src/main/java/pl/wsb/hotel/SpecialService.java
+++ Hotel/src/main/java/pl/wsb/hotel/SpecialService.java
@@ -3,11 +3,10 @@ package pl.wsb.hotel;
 public abstract class SpecialService {
     private String name;
     public abstract void orderService();
-    // additional extra services //
-    public abstract void orderClothesCleaning();
-    public abstract void orderNetflix();
-
-    public String getServiceName(){
+    public abstract void helpService();
+    public abstract void payForService();
+    public abstract void claimService();
+    public String getServiceName() {
         return name;
     }
 }
