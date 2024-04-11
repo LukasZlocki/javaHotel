4c725ba3a1f88b0aa66b823d9ac7298001c784a0
lzlocki <lzlocki@poczta.fm>
2024-04-11 18:01:16 +0200

    Add special services class

---
+++ Hotel/src/main/java/pl/wsb/hotel/SpecialService.java
@@ -0,0 +1,9 @@
+package pl.wsb.hotel;
+
+public abstract class SpecialService {
+    String name;
+    abstract void orderService();
+    // additional extra services //
+    abstract void orderClothesCleaning();
+    abstract void orderNetflix();
+}
