1971f9b6cf762a3ab0fe7b00f29f1253ff783403
lzlocki <lzlocki@poczta.fm>
2024-04-11 22:47:25 +0200

    remove old add new services

--- Hotel/src/main/java/pl/wsb/hotel/LuggageService.java
+++ Hotel/src/main/java/pl/wsb/hotel/LuggageService.java
@@ -1,18 +1,24 @@
 package pl.wsb.hotel;
 
-public class LuggageService extends SpecialService{
+public class LuggageService extends SpecialService {
     @Override
     public void orderService() {
-        System.out.println("Your Luggage is in our hotel.");
+        System.out.println("Your Luggage is in our hotel now.");
     }
 
     @Override
-    public void orderClothesCleaning() {
-        System.out.println("Your ordered cleaning of clothes.");
+    public void helpService() {
+        // add code here ... //
     }
 
     @Override
-    public void orderNetflix() {
-        System.out.println("Your ordered Netflix.");
+    public void payForService() {
+        // add code here ... //
     }
+
+    @Override
+    public void claimService() {
+        // add code here ... //
+    }
+
 }
--- Hotel/src/main/java/pl/wsb/hotel/TimeService.java
+++ Hotel/src/main/java/pl/wsb/hotel/TimeService.java
@@ -2,19 +2,24 @@ package pl.wsb.hotel;
 
 import java.time.LocalTime;
 
-public class TimeService extends SpecialService{
+public class TimeService extends SpecialService {
     @Override
     public void orderService() {
         System.out.println("Actual time: " + LocalTime.now());
     }
 
     @Override
-    public void orderClothesCleaning() {
-        // code body here... //
+    public void helpService() {
+        // add code here ... //
     }
 
     @Override
-    public void orderNetflix() {
-        // code body here... //
+    public void payForService() {
+        // add code here ... //
+    }
+
+    @Override
+    public void claimService() {
+        // add code here ... //
     }
 }
