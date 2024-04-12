c24f9420506e7f7f006905f6b52015a7babdb10a
lzlocki <lzlocki@poczta.fm>
2024-04-12 18:48:14 +0200

    Add constructor to luggage service

--- Hotel/src/main/java/pl/wsb/hotel/SpecialService.java
+++ Hotel/src/main/java/pl/wsb/hotel/SpecialService.java
@@ -2,6 +2,12 @@ package pl.wsb.hotel;
 
 public abstract class SpecialService {
     private String name;
+
+    // constructor //
+    public SpecialService(String name){
+        this.name = name;
+    }
+
     public abstract void orderService();
     public abstract void helpService();
     public abstract void payForService();
