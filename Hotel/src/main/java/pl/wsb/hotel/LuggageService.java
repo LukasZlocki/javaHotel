c47221755cb98cfa3ba93d2eded86ccc2f29b140
lzlocki <lzlocki@poczta.fm>
2024-04-12 18:48:44 +0200

    Add constructor to luggage service

--- Hotel/src/main/java/pl/wsb/hotel/LuggageService.java
+++ Hotel/src/main/java/pl/wsb/hotel/LuggageService.java
@@ -1,6 +1,12 @@
 package pl.wsb.hotel;
 
 public class LuggageService extends SpecialService {
+
+    // constructor //
+    public LuggageService(String name){
+        super(name);
+    }
+
     @Override
     public void orderService() {
         System.out.println("Your Luggage is in our hotel now.");
