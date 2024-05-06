962d29cfb8beffffc8f53b6d5b5682593c66edef
lzlocki <lzlocki@poczta.fm>
2024-05-06 20:46:50 +0200

    Add random class library

--- Hotel/src/main/java/pl/wsb/hotel/Hotel.java
+++ Hotel/src/main/java/pl/wsb/hotel/Hotel.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 public class Hotel implements HotelCapability {
     private String name;
