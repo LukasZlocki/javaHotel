00e821fbe4282aa73052eae699a1a114532c0355
lzlocki <lzlocki@poczta.fm>
2024-05-06 19:21:02 +0200

    Implement interface with empty methods

--- Hotel/src/main/java/pl/wsb/hotel/Hotel.java
+++ Hotel/src/main/java/pl/wsb/hotel/Hotel.java
@@ -1,9 +1,17 @@
 package pl.wsb.hotel;
 
+import pl.wsb.hotel.exceptions.ClientNotFoundException;
+import pl.wsb.hotel.exceptions.ReservationNotFoundException;
+import pl.wsb.hotel.exceptions.RoomNotFoundException;
+import pl.wsb.hotel.exceptions.RoomReservedException;
+import pl.wsb.hotel.interfaces.HotelCapability;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-public class Hotel {
+public class Hotel implements HotelCapability {
     private String name;
     private List<SpecialService> specialServices;
     private List<Client> clients;
@@ -26,5 +34,60 @@ public class Hotel {
         this.clients = clients;
         this.reservations = reservations;
         this.rooms = rooms;
+    }
+
+    @Override
+    public String addClient(String firstName, String lastName, LocalDate birthDate) {
+        return null;
+    }
+
+    @Override
+    public String getClientFullName(String clientId) {
+        return null;
+    }
+
+    @Override
+    public int getNumberOfUnderageClients() {
+        return 0;
+    }
+
+    @Override
+    public String addRoom(double area, int floor, boolean hasKingSizeBed, String description) {
+        return null;
+    }
+
+    @Override
+    public double getRoomArea(String roomId) {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfRoomsWithKingSizeBed(int floor) {
+        return 0;
+    }
+
+    @Override
+    public String addNewReservation(String clientId, String roomId, LocalDate date) throws ClientNotFoundException, RoomNotFoundException, RoomReservedException {
+        return null;
+    }
+
+    @Override
+    public String confirmReservation(String reservationId) throws ReservationNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean isRoomReserved(String roomId, LocalDate date) throws RoomNotFoundException {
+        return false;
+    }
+
+    @Override
+    public int getNumberOfUnconfirmedReservation(LocalDate date) {
+        return 0;
+    }
+
+    @Override
+    public Collection<String> getRoomIdsReservedByClient(String clientId) throws ClientNotFoundException {
+        return null;
     }
 }
