3808779ca7eea8de3efe2a341b5841119ddc5b1c
lzlocki <lzlocki@poczta.fm>
2024-05-09 22:43:18 +0200

    Add all tests to cover 94% of hotel class code

--- Hotel/src/test/java/pl/wsb/hotel/HotelTest.java
+++ Hotel/src/test/java/pl/wsb/hotel/HotelTest.java
@@ -8,25 +8,20 @@ import pl.wsb.hotel.exceptions.RoomNotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HotelTest {
-    Room room1 = new Room("1111", "room1", 25.10, 1, true, true, true, 1, true, true );
-    Room room2 = new Room("2222", "room2", 50.10, 2, true, true, true, 2, true, true );
-    Room room3 = new Room("3333", "room13", 75.10, 3, false, false, false, 3, false, false );
-    Client client1 = new Client("11", "client1firstname", "client1lastname", LocalDate.of(2019, Month.AUGUST, 11), "Poland", 11111111, "client1@thismail.com");
-    Client client2 = new Client("22", "client2firstname", "client2lastname", LocalDate.of(1999, Month.AUGUST, 12), "England", 22222222, "client2@thismail.com");
-    Client client3 = new Client("33", "client3firstname", "client3lastname", LocalDate.of(1969, Month.AUGUST, 13), "USA", 33333333, "client3@thismail.com");
-    Hotel hotel1 = new Hotel("hotel1");
-    String clientId;
+
 
     @Test
     void testAddingClientAndCheckIfGeneratedIdCoversAddedClient(){
         // Arrange //
         Hotel hotel = new Hotel("hotel");
-        clientId = hotel.addClient("client1firstname", "client1lastname", LocalDate.of(1997, Month.AUGUST, 12));
+        String clientId = hotel.addClient("client1firstname", "client1lastname", LocalDate.of(1997, Month.AUGUST, 12));
         // Assert //
         assertEquals("client1firstname client1lastname", hotel.getClientFullName(clientId));
     }
@@ -42,38 +37,44 @@ public class HotelTest {
 
     @Test
     void testNullWhenNoClientWithGivenClientId(){
+        // Arrange //
+        Hotel hotel = new Hotel("hotel");
         // Assert //
-        Assertions.assertNull(null, hotel1.getClientFullName("falseClientId"));
+        Assertions.assertNull(null, hotel.getClientFullName("falseClientId"));
     }
 
     @Test
     void testCalculatingUnderageClients(){
         // Arrange //
-        String underageClientId = hotel1.addClient("clientunderagefirstname", "clientunderagelastname", LocalDate.of(2020, Month.AUGUST, 12));
+        Hotel hotel = new Hotel("hotel");
+        String underageClientId = hotel.addClient("clientunderagefirstname", "clientunderagelastname", LocalDate.of(2020, Month.AUGUST, 12));
         // Assert //
-        assertEquals(1, hotel1.getNumberOfUnderageClients());
+        assertEquals(1, hotel.getNumberOfUnderageClients());
     }
 
     @Test
     void testAddRoomToHotelAndCheckIfGeneratedIdCoversAddedRoom(){
         // Arrange //
-        String roomId = hotel1.addRoom(99.12, 5, false, "testroom");
+        Hotel hotel = new Hotel("hotel");
+        String roomId = hotel.addRoom(99.12, 5, false, "testroom");
         // Assert //
-        assertEquals(99.12, hotel1.getRoomArea(roomId));
+        assertEquals(99.12, hotel.getRoomArea(roomId));
     }
 
     @Test
     void testGetRoomAreaByRoomId(){
         // Arrange //
-        String areaRoomId = hotel1.addRoom(100.20, 3, false, "testroomarea");
+        Hotel hotel = new Hotel("hotel");
+        String areaRoomId = hotel.addRoom(100.20, 3, false, "testroomarea");
         // Assert //
-        assertEquals(100.20, hotel1.getRoomArea(areaRoomId));
+        assertEquals(100.20, hotel.getRoomArea(areaRoomId));
     }
 
     @Test
     void testGetRoomAreaByFalseRoomId(){
         // Assert //
-        assertEquals(0, hotel1.getRoomArea("falseRoomId"));
+        Hotel hotel = new Hotel("hotel");
+        assertEquals(0, hotel.getRoomArea("falseRoomId"));
     }
 
     @Test
@@ -99,43 +100,26 @@ public class HotelTest {
     @Test
     void testAddReservationWithNoValidCustomerId(){
         // Arrange
+        Hotel hotel = new Hotel("hotel");
         ClientNotFoundException exception = assertThrows(ClientNotFoundException.class, () -> {
-            hotel1.addNewReservation("falseClientId", "falseRoomId", LocalDate.of(2020, Month.AUGUST, 12));
+            hotel.addNewReservation("falseClientId", "falseRoomId", LocalDate.of(2020, Month.AUGUST, 12));
         });
         // Assert //
         assertEquals("Client not found in database.", exception.getMessage());
     }
 
-    /* ToDo: fix test here
-    @Test
-    void testAddReservationWithNoValidRoomId(){
-        // Arrange
-        Hotel hotelReservation1 = new Hotel("reservationTest");
-        String reservationClientId = hotelReservation1.addClient("client1firstname", "client1lastname", LocalDate.of(1997, Month.AUGUST, 12));
-        RoomNotFoundException exception1 = assertThrows(RoomNotFoundException.class, () -> {
-            hotel1.addNewReservation(reservationClientId , "falseRoomId", LocalDate.of(2020, Month.AUGUST, 12));
-        });
-        // Assert //
-        assertEquals("Room not found in database.", exception1.getMessage());
-    }
 
     @Test
-    void testAddReservationWithNoAvailableRoomAtSpecificDate(){
+    void testAddReservationWithNoValidRoomId() throws RoomNotFoundException{
         // Arrange
-        LocalDate specificDate = LocalDate.of(2021, Month.AUGUST, 21);
-        Hotel hotelReservation2 = new Hotel("reservationTest2");
-        String roomId = hotelReservation2.addRoom(45.20, 1, false, "testroomarea");
-        String clientId = hotelReservation2.addClient("client", "client", LocalDate.of(2020, Month.AUGUST, 12));
-        String reservationId = hotelReservation2.addNewReservation(clientId, roomId, specificDate);
-
-        String reservationClientId = hotelReservation2.addClient("client1firstname", "client1lastname", LocalDate.of(1997, Month.AUGUST, 12));
-        RoomNotFoundException exception1 = assertThrows(RoomNotFoundException.class, () -> {
-            hotel1.addNewReservation(reservationClientId , "falseRoomId", specificDate);
+        Hotel hotel = new Hotel("reservationTest");
+        String clientId = hotel.addClient("client1firstname", "client1lastname", LocalDate.of(1997, Month.AUGUST, 12));
+        RoomNotFoundException exception = assertThrows(RoomNotFoundException.class, () -> {
+            hotel.addNewReservation(clientId , "falseRoomId", LocalDate.of(2020, Month.AUGUST, 12));
         });
         // Assert //
-        assertEquals("Room not found in database.", exception1.getMessage());
+        assertEquals("Room not found in database.", exception.getMessage());
     }
-    */
 
     @Test
     void testConfirmReservationByFailedReservationId(){
@@ -161,6 +145,68 @@ public class HotelTest {
         // Act & Assert //
         assertEquals(reservationId, hotel.confirmReservation(reservationId));
     }
+
+    @Test
+    void testIsRoomReservedByGoodRoomIdAndDateWhereRoomIsNotAvailable() throws ClientNotFoundException, RoomNotFoundException {
+        // Arrange //
+        Client client1 = new Client("11", "client1firstname", "client1lastname", LocalDate.of(2019, Month.AUGUST, 11), "Poland", 11111111, "client1@thismail.com");
+        Room room1 = new Room("1111", "room1", 25.10, 1, true, true, true, 1, true, true );
+        Hotel hotel = new Hotel("Reservation Test");
+        String clientId = hotel.addClient("client1firstname", "client1lastname", LocalDate.of(2019, Month.AUGUST, 11));
+        String roomId = hotel.addRoom(45.20, 1, true, "testroomarea");
+        String reservationId = hotel.addNewReservation(clientId, roomId,LocalDate.of(2023, Month.AUGUST, 11));
+
+        // Assert
+        assertTrue(hotel.isRoomReserved(roomId, LocalDate.of(2023, Month.AUGUST, 11)));
+    }
+
+    @Test
+    void testIsRoomReservedByGoodRoomIdAndDateWhereRoomIsAvailable() throws ClientNotFoundException, RoomNotFoundException {
+        // Arrange //
+        Client client1 = new Client("11", "client1firstname", "client1lastname", LocalDate.of(2019, Month.AUGUST, 11), "Poland", 11111111, "client1@thismail.com");
+        Room room1 = new Room("1111", "room1", 25.10, 1, true, true, true, 1, true, true );
+        Hotel hotel = new Hotel("Reservation Test");
+        String clientId = hotel.addClient("client1firstname", "client1lastname", LocalDate.of(2019, Month.AUGUST, 11));
+        String roomId = hotel.addRoom(45.20, 1, true, "testroomarea");
+        String reservationId = hotel.addNewReservation(clientId, roomId,LocalDate.of(2022, Month.AUGUST, 11));
+
+        // Assert
+        assertFalse(hotel.isRoomReserved(roomId, LocalDate.of(2023, Month.AUGUST, 11)));
+    }
+
+    @Test
+    void testGettingNumbersOfUnconfirmedReservations() throws ClientNotFoundException, RoomNotFoundException {
+        // Arrange //
+        Client client1 = new Client("11", "client1firstname", "client1lastname", LocalDate.of(2019, Month.AUGUST, 11), "Poland", 11111111, "client1@thismail.com");
+        Room room1 = new Room("1111", "room1", 25.10, 1, true, true, true, 1, true, true );
+        Hotel hotel = new Hotel("Reservation Test");
+        String clientId = hotel.addClient("client1firstname", "client1lastname", LocalDate.of(2019, Month.AUGUST, 11));
+        String roomId = hotel.addRoom(45.20, 1, true, "testroomarea");
+        String reservationId = hotel.addNewReservation(clientId, roomId,LocalDate.of(2022, Month.AUGUST, 11));
+
+        // Assert
+        assertEquals(1, hotel.getNumberOfUnconfirmedReservation(LocalDate.of(2022, Month.AUGUST, 11)));
+    }
+
+    @Test
+    void testGetRoomIdsReservedByClient() throws ClientNotFoundException, RoomNotFoundException {
+        // Arrange //
+        Hotel hotel = new Hotel("Reservation Test");
+        String clientId = hotel.addClient("client1firstname", "client1lastname", LocalDate.of(2019, Month.AUGUST, 11));
+        String roomId = hotel.addRoom(45.20, 1, true, "testroomarea");
+        String reservationId1 = hotel.addNewReservation(clientId, roomId,LocalDate.of(2022, Month.AUGUST, 11));
+        String reservationId2 = hotel.addNewReservation(clientId, roomId,LocalDate.of(2021, Month.AUGUST, 11));
+        String reservationId3 = hotel.addNewReservation(clientId, roomId,LocalDate.of(2020, Month.AUGUST, 11));
+
+        // Act //
+        Collection<String> clientRoomsIds = new ArrayList<>();
+        clientRoomsIds = hotel.getRoomIdsReservedByClient(clientId);
+
+        // Assert //
+        assertEquals(3, hotel.getRoomIdsReservedByClient(clientId).size());
+    }
+
+
 }
 
 
